44396ba9a2cb0409ff383540445aca3d5d242ccf
Silvanei Santos <silvanei.santos@objective.com.br>
2023-12-16 14:16:59 -0300

    Add users facade

--- lib/elixir_pdi_phoenix_api_web/controllers/users_controller.ex
+++ lib/elixir_pdi_phoenix_api_web/controllers/users_controller.ex
@@ -1,13 +1,13 @@
 defmodule ElixirPdiPhoenixApiWeb.UsersController do
   use ElixirPdiPhoenixApiWeb, :controller
 
-  alias ElixirPdiPhoenixApi.Users.Create
+  alias ElixirPdiPhoenixApi.Users
   alias ElixirPdiPhoenixApi.Users.User
 
   action_fallback ElixirPdiPhoenixApiWeb.FallbackController
 
   def create(conn, params) do
-    with {:ok, %User{} = user} <- Create.call(params) do
+    with {:ok, %User{} = user} <- Users.create(params) do
       conn
       |> put_status(:created)
       |> render(:create, user: user)
