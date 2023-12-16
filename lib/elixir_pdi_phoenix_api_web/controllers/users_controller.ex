3051cea68f04dd45602800a4ce3eef0a0ac0197d
Silvanei Santos <silvanei.santos@objective.com.br>
2023-12-16 14:07:13 -0300

    Add falback controller for error

--- lib/elixir_pdi_phoenix_api_web/controllers/users_controller.ex
+++ lib/elixir_pdi_phoenix_api_web/controllers/users_controller.ex
@@ -2,23 +2,15 @@ defmodule ElixirPdiPhoenixApiWeb.UsersController do
   use ElixirPdiPhoenixApiWeb, :controller
 
   alias ElixirPdiPhoenixApi.Users.Create
+  alias ElixirPdiPhoenixApi.Users.User
+
+  action_fallback ElixirPdiPhoenixApiWeb.FallbackController
 
   def create(conn, params) do
-    params
-    |> Create.call()
-    |> handle_response(conn)
-  end
-
-  defp handle_response({:ok, user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render(:create, user: user)
-  end
-
-  defp handle_response({:error, changeset}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(json: ElixirPdiPhoenixApiWeb.ErrorJSON)
-    |> render(:error, changeset: changeset)
+    with {:ok, %User{} = user} <- Create.call(params) do
+      conn
+      |> put_status(:created)
+      |> render(:create, user: user)
+    end
   end
 end
