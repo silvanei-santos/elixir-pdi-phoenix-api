6215180502b11a055804ea6b7a92010278cd8179
Silvanei Santos <silvanei.santos@objective.com.br>
2023-12-16 21:45:16 -0300

    Add users update

---
+++ lib/elixir_pdi_phoenix_api/users/update.ex
@@ -0,0 +1,17 @@
+defmodule ElixirPdiPhoenixApi.Users.Update do
+  alias ElixirPdiPhoenixApi.Users.User
+  alias ElixirPdiPhoenixApi.Repo
+
+  def call(%{"id" => id} = params) do
+    case Repo.get(User, id) do
+      nil -> {:error, :not_found}
+      user -> update(user, params)
+    end
+  end
+
+  defp update(user, params) do
+    user
+    |> User.changeset(params)
+    |> Repo.update()
+  end
+end
