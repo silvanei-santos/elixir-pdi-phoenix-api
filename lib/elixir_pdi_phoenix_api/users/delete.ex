9cacd6636bf4894893bad921927c70be2bd2ce9f
Silvanei Santos <silvanei.santos@objective.com.br>
2023-12-23 16:40:07 -0300

    Upkeep delete user with account

--- lib/elixir_pdi_phoenix_api/users/delete.ex
+++ lib/elixir_pdi_phoenix_api/users/delete.ex
@@ -1,11 +1,32 @@
 defmodule ElixirPdiPhoenixApi.Users.Delete do
+  alias Ecto.Multi
+
   alias ElixirPdiPhoenixApi.Users.User
   alias ElixirPdiPhoenixApi.Repo
 
   def call(id) do
     case Repo.get(User, id) do
-      nil -> {:error, :not_found}
-      user -> Repo.delete(user)
+      nil ->
+        {:error, :not_found}
+
+      user ->
+        Multi.new()
+        |> delete_account(user)
+        |> delete_user(user)
+        |> Repo.transaction()
+        |> handle_transaction()
     end
   end
+
+  defp delete_account(multi, user) do
+    case Repo.preload(user, :account) do
+      %User{account: nil} -> multi
+      %User{account: account} -> Multi.delete(multi, :delete_user_account, account)
+    end
+  end
+
+  defp delete_user(multi, user), do: Multi.delete(multi, :delete_user, user)
+
+  defp handle_transaction({:ok, %{delete_user: user}}), do: {:ok, user}
+  defp handle_transaction({:error, _op, reason, _}), do: {:error, reason}
 end
